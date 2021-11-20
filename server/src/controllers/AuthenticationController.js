158c30efc55d618fda92badd03afb7c45d8cc9dc
Svaagi <jozefsvagerkom5@gmail.com>
2021-11-20 18:17:44 +0100

    added password hash

--- server/src/controllers/AuthenticationController.js
+++ server/src/controllers/AuthenticationController.js
@@ -13,7 +13,9 @@ module.exports = {
   async register (req, res) {
     try {
       const user = await User.create(req.body)
-      res.send(user.toJSON())
+      res.status(200).send({
+        userId: user.id
+      })
     } catch (err) {
       res.status(400).send({
         error: 'This email account is already in use'
@@ -34,7 +36,7 @@ module.exports = {
         })
       }
 
-      const isPasswordValid = password === user.password
+      const isPasswordValid = user.comparePassword(password)
       if (!isPasswordValid) {
         return res.status(403).send({
           error: 'The login information was incorrect'
