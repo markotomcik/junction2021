6cded962eb09a50f583cc3fcb564f50a1bc9e7b6
Svaagi <jozefsvagerkom5@gmail.com>
2021-11-20 11:56:52 +0100

    db init

---
+++ server/src/config/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  port: process.env.PORT || 8081,
+  db: {
+    database: process.env.DB_NAME || 'junction2021',
+    user: process.env.DB_USER || 'junction2021',
+    password: process.env.DB_PASS || 'lol',
+    options: {
+      dialect: process.env.DIALECT || 'sqlite',
+      host: process.env.HOST || 'localhost'
+    }
+  }
+}
