fd9e2f9ba9faebd256195d326b9c6526f56e0c40
Marko Tomčík <markotomcik@gmail.com>
2021-11-20 15:51:28 +0100

    Box model, controller, routes

---
+++ server/src/controllers/BoxController.js
@@ -0,0 +1,54 @@
+module.exports = {
+  async new (req, res) {
+    try {
+      const box = await Box.create(req.body)
+      return res.status(201).json(box)
+    } catch (err) {
+      return res.status(400).json({
+        error: 'The creaction of new box failed.'
+      })
+    }
+  },
+
+  async get(req, res) {
+    try {
+      const box = await Box.findById(req.params.id)
+      return res.status(200).json(box)
+    } catch (err) {
+      return res.status(400).json({
+        error: 'The box was not found.'
+      })
+    }
+  },
+
+  async getInRadius(req, res) {
+    try {
+      const boxes = await Box.find({
+        where: {
+          locationX: {
+            [Op.between]: [req.params.locationX - req.params.radius, req.params.locationX + req.params.radius]
+          },
+          locationY: {
+            [Op.between]: [req.params.locationY - req.params.radius, req.params.locationY + req.params.radius]
+          }
+        }
+      })
+      return res.status(200).json(boxes)
+    } catch (err) {
+      return res.status(400).json({
+        error: 'The boxes were not found.'
+      })
+    }
+  },
+
+  async update (req, res) {
+    try {
+      const box = await Box.findByIdAndUpdate(req.params.id, req.body, { new: true })
+      return res.status(200).json(box)
+    } catch (err) {
+      return res.status(400).json({
+        error: 'The update of box failed.'
+      })
+    }
+  }
+}
--- server/src/routes.js
+++ server/src/routes.js
@@ -1,4 +1,5 @@
 const AuthenticationController = require('./controllers/AuthenticationController')
+const BoxController = require('./controllers/BoxController')
 
 // const AuthenticationControllerPolicy = require('./policies/AuthenticationControllerPolicy')
 
@@ -6,4 +7,17 @@ module.exports = (app) => {
   app.post('/register',
     // AuthenticationControllerPolicy.register,
     AuthenticationController.register)
+
+  // Box Routes
+  app.post('/box/new',
+    BoxController.new)
+
+  app.get('/box/:id',
+    BoxController.get)
+
+  app.get('/box/:locationX/:locationY/:range',
+    BoxController.getInRadius)
+
+  app.put('/box/:id',
+    BoxController.update)
 }
