cdeebfc65864d3d8d6090d6a4cf36a55016e1cf9
Svaagi <jozefsvagerkom5@gmail.com>
2021-11-20 17:16:08 +0100

    login added token and so on :3

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -9,5 +9,8 @@ module.exports = {
       host: process.env.HOST || 'localhost',
       storage: './junction2021.sqlite'
     }
+  },
+  authentication: {
+    jwtSecret: process.env.JWT_SECRET || 'secret'
   }
 }
